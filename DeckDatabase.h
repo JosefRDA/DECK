c580e5ca29e2f2afea7b85ec4da7f2beddbccc90
JosefRDA <josef.rda@gmail.com>
2021-09-23 08:53:06 +0200

    update DeckDatabase

--- DeckDatabase.h
+++ DeckDatabase.h
@@ -1,4 +1,6 @@
-#include <LittleFS.h>
+#ifndef _DECKDATABASE_H_
+#define _DECKDATABASE_H_
+
 #include <ArduinoJson.h> // arduino json v6  // https://github.com/bblanchon/ArduinoJson
 
 // to upload config dile : https://github.com/earlephilhower/arduino-esp8266littlefs-plugin/releases
@@ -7,113 +9,26 @@
 
 class DeckDatabase {
   public:
-  DeckDatabase() {
-    
-  }
-
-  void mountFS() {
-    Serial.println(F("Mount LittleFS"));
-    if (!LittleFS.begin())
-    {
-      Serial.println(F("LittleFS mount failed"));
-      return;
-    }
-  }
-
-  void printJsonFile(const char * filename)
-  {
-    // Open file for reading
-    File file = LittleFS.open(filename, "r");
-    if (!file) 
-    {
-      Serial.println(F("Failed to open file for reading"));
-    }
-      
-    StaticJsonDocument<1024> doc;
-    
-    // Deserialize the JSON document
-    DeserializationError error = deserializeJson(doc, file);
-    if (error)
-    {
-      Serial.println(F("Failed to deserialize file"));
-      Serial.println(error.c_str());
-    }
-    else
-    {
-      serializeJsonPretty(doc, Serial);
-      Serial.println();
-    }
-    
-    // Close the file (File's destructor doesn't close the file)
-    file.close();
-  }
-
-  void listDir(const char * dirname)
-  {
-    Serial.printf("Listing directory: %s", dirname);
-    Serial.println();
   
-    Dir root = LittleFS.openDir(dirname);
+  // CONSTRUCTORS ------------------------------------------------------------
   
-    while (root.next())
-    {
-      File file = root.openFile("r");
-      Serial.print(F("  FILE: "));
-      Serial.print(root.fileName());
-      Serial.print(F("  SIZE: "));
-      Serial.print(file.size());
-      Serial.println();
-      file.close();
-    }
+  DeckDatabase();
+  
+  // CLASS MEMBER FUNCTIONS ----------------------------------------------
 
-    Serial.println();
-  }
+  void mountFS(void);
 
-  String getLabelByUid(const char * filename, String uid) {
-    JsonObject stim = getStimByUid(filename, uid);
-    if(stim) {
-      return stim["label"].as<char*>();
-    } else {
-      return String("[NULL]");
-    }
-  }
+  void printJsonFile(const char * filename);
 
-  JsonObject getStimByUid(const char * filename, String uid)
-  {
-    JsonObject result;
-    
-    // Open file for reading
-    File file = LittleFS.open(filename, "r");
-    if (!file) 
-    {
-      Serial.println(F("Failed to open file for reading"));
-    }
-      
-    StaticJsonDocument<1024> doc;
-    
-    // Deserialize the JSON document
-    DeserializationError error = deserializeJson(doc, file);
-    if (error)
-    {
-      Serial.println(F("Failed to deserialize file"));
-      Serial.println(error.c_str());
-    }
-    else
-    {
-      JsonArray stimArray = doc.as<JsonArray>();
-      for(JsonVariant value : stimArray) {
-        JsonObject stimValue = value.as<JsonObject>();
-        if(uid.equals(stimValue["uid"].as<char*>())) {
-          result = stimValue;
-        }
-      }
-    }
-    
-    // Close the file (File's destructor doesn't close the file)
-    file.close();
+  void listDir(const char * dirname);
 
-    return result;
-  }
+  String getLabelByUid(const char * filename, String uid);
+
+  JsonObject getStimByUid(const char * filename, String uid);
   
   
 };
+
+
+
+#endif // end _DECKDATABASE_H_
